8abffa5b1a2a9be9cd033398b042df2251c7b623
Artem311982 <110343485+Artem311982@users.noreply.github.com>
2023-02-17 22:00:24 +0200

    170223

--- js/slider.js
+++ js/slider.js
@@ -1,18 +1,7 @@
-$('.filtering').slick({
-  slidesToShow: 4,
-  slidesToScroll: 4,
-});
-
-var filtered = false;
-
-$('.js-filter').on('click', function () {
-  if (filtered === false) {
-    $('.filtering').slick('slickFilter', ':even');
-    $(this).text('Unfilter Slides');
-    filtered = true;
-  } else {
-    $('.filtering').slick('slickUnfilter');
-    $(this).text('Filter Slides');
-    filtered = false;
-  }
+$('.one-time').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
 });
